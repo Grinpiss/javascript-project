4159556a70e23fd64d2496f4e67c131d648e0889
Gringo <vetal6-13-23@mail.ru>
2024-06-23 18:25:00 +0300

    feat addEventListener onclick

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ document.addEventListener('keydown' , (event) => {
 }
 })
 
+document.addEventListener('click' , (event) => {
+  const type = event.target.dataset.type
+
+  if (type === 'lock') {
+    const node = 
+     event.target.tagName.toLowerCase() ===  'i'
+     ? event.target
+     : event.target.children [0]
+    
+     node.classList.toggle('fa-lock-open')
+     node.classList.toggle('fa-lock')
+    }
+  })
+
 function generateRandomColor() {
   // RGB
   // #FF0000
