c41be89deee95f6c68f8df516c41ffaffc6969a0
Gringo <vetal6-13-23@mail.ru>
2024-06-23 14:36:12 +0300

    adв  js func

---
+++ index.js
@@ -0,0 +1,17 @@
+const cols = document.querySelectorAll('.col')
+
+function generateRandomColor() {
+  // RGB
+  // #FF0000
+  // #00FF00
+  // #0000FF
+
+  const hexCodes = '0123456789ABCDEF'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes [Mat]
+  }
+
+
+ 
+}
